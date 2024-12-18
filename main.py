95067a27d3648654a35d30097627c719dc069c5d
carlmenke <carlmenke13@gmail.com>
2024-12-17 21:08:25 -0800

    add window

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
+from tkinter import *
 
-print('carl was here')
-print('Test')
+window = Tk()
+window.title('Gubs')
+window.configure(background='#5c4d42')
+
+photo1 = PhotoImage(file='gub_card.png')
+Label(window, image=photo1, background='#5c4d42').grid(row=0, column=0, sticky=W)
+
+window.mainloop()
+
 
