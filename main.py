75f75359c777724cc6277a1e7899624b0e2b60ba
JulianC775 <juliancatello4@gmail.com>
2024-12-17 21:27:09 -0800

    prep planning

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ Label(window, image=photo1, background='#5c4d42').grid(row=0, column=0, sticky=W
 window.mainloop()
 
 
+"""
+players: cards in hand, cards in play
+deck of cards: drawcard
+
+gubs: move to discard pile. add to total points
+spear:moves a gub or protection card to discard
+mushroom: protect gub
+toad ridaaaaaa: protect gub 
+discard pile:
+
+event cards: when drawn -> used
+flashflood: discard all naked gubs
+G U B cards: win logic, if tie, lowest ammount of cards in hand wins
+
+
+"""
